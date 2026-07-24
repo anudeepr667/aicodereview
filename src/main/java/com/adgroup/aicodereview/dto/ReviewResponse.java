1a887a74d10dedf6dbe899ca6e90b8ad96341f60
vedavyask-wq <vedavyask@kssem.edu.in>
2026-07-24 11:50:01 +0530

    Added DTO implementation and environment variable configuration

---
+++ src/main/java/com/adgroup/aicodereview/dto/ReviewResponse.java
@@ -0,0 +1,53 @@
+package com.adgroup.aicodereview.dto;
+
+public class ReviewResponse {
+
+    private Long id;
+    private String repositoryName;
+    private String branchName;
+    private String pullRequestId;
+    private String reviewText;
+
+    public ReviewResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+
+    public void setRepositoryName(String repositoryName) {
+        this.repositoryName = repositoryName;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getPullRequestId() {
+        return pullRequestId;
+    }
+
+    public void setPullRequestId(String pullRequestId) {
+        this.pullRequestId = pullRequestId;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+}
